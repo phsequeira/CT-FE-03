6ce27803bc5502436a472b65f3ea6e6e21b19d6e
Parker Sequeira <phsequeira@yahoo.com>
2021-05-12 16:56:46 -0700

    important stuff

--- src/services/newsApi.js
+++ src/services/newsApi.js
@@ -1,12 +1,12 @@
 const topHeadLinesUS = async () => {
-    const res = await fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=a1169c23523b49dd918609503a5609c8');
+    const res = await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.newsapiKey}`);
     const results = await res.json();
     const arts = results.articles;
     return arts;
 }
 
 const findArticle = async (topic) => {
-    const res = await fetch(`https://newsapi.org/v2/everything?q=${topic}&apiKey=a1169c23523b49dd918609503a5609c8`)
+    const res = await fetch(`https://newsapi.org/v2/everything?q=${topic}&apiKey=${process.env.newsapiKey}`)
     const results = await res.json();
     const arts = results.articles;
     return arts;
